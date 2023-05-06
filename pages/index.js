a950b497ec07ee2b3c5a13dfee2b4ca3641a2be2
Vitor Afonso <vittokm@hotmail.com>
2023-05-06 12:01:01 +0100

    Add dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { name, siteTitle } from 'components/layout';
 import utilStyles from 'styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from 'components/date';
 
 // STATIC GENERATION WITH DATA
 // This page will only be generated once, at runtimme, in the server and will be reused at every request
@@ -23,6 +25,14 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      {/* Example on how to introduce a script on a specific page */}
+      {/* <Script
+        src='https://connect.facebook.net/en_US/sdk.js'
+        strategy='lazyOnload'
+        onLoad={() => {
+          console.log(`script loaded correctly, window.FB has been populated`, window.FB);
+        }}
+      /> */}
       <section className={utilStyles.headingMd}>
         <p>Hello! I'm {name}, a dev with experience in JS and React. I'm excited to share my insights on this blog and inspire others in tech.</p>
         <p>
@@ -34,11 +44,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
---
+++ pages/posts/[id].js
@@ -0,0 +1,54 @@
+import Layout from 'components/layout';
+import { getAllPostIds, getPostData } from 'lib/posts';
+import Date from 'components/date';
+import Head from 'next/head';
+import utilStyles from 'styles/utils.module.css';
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  /* Important: The returned list is not just an array of strings — it must be an array of objects that look like the comment above. Each object must have the params key and contain an object with the id key (because we’re using [id] in the file name). Otherwise, getStaticPaths will fail. */
+
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
