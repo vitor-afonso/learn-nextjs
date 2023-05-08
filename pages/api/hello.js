165e21af223eb62743f927aceb6e8f2e4b977bd2
Vitor Afonso <vittokm@hotmail.com>
2023-05-08 10:05:32 +0100

    Add API routes example

---
+++ pages/api/hello.js
@@ -0,0 +1,16 @@
+// Next.js has support for API Routes, which let you easily create an API endpoint as a Node.js serverless function.
+
+// This endpoint would be: http://localhost:3000/api/hello
+
+// You should not fetch an API Route from getStaticProps or getStaticPaths.
+//Instead, write your server-side code directly in getStaticProps or getStaticPaths
+
+// To handle different HTTP methods in an API route, you can use req.method in your request handler, like so:
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    res.status(200).json({ text: 'Hello POST' });
+  } else {
+    res.status(200).json({ text: 'Hello GET' });
+  }
+}
